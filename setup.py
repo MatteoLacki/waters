464c47d33291a9e675f03f6f08d8a8caca2c3fbc
MatteoLacki <matteo.lacki@gmail.com>
2020-02-12 15:12:55 +0100

    added OS specific scripts setup

--- setup.py
+++ setup.py
@@ -1,32 +1,29 @@
 # This Python file uses the following encoding: utf-8
 from setuptools import setup, find_packages
+import platform
 
-setup(
-    name='waters',
-    packages=find_packages(),
-    version='0.0.1',
-    description='Parsing MS data from Waters.',
-    long_description='Parsing MS data from Waters.',
-    author='Mateusz Krzysztof Łącki',
-    author_email='matteo.lacki@gmail.com',
-    url='https://github.com/MatteoLacki/waters/',
-    # download_url='https://github.com/MatteoLacki/MassTodonPy/tree/GutenTag',
-    keywords=['Mass Spectrometry',
-              'Waters'],
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'License :: OSI Approved :: BSD License',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Chemistry',
-        'Programming Language :: Python :: 3.7'],
-    install_requires=['pandas'],
-    # include_package_data=True,
-    # package_data={
-    #     'data':
-    #          ['data/contaminants.fasta']
-    # },
-    scripts = ["bin/iadbs2stats",
-               "bin/iadbs2csv",
-               "bin/iadbs2stats.py",
-               "bin/iadbs2csv.py"]
-)
+settings = dict(name='waters',
+                packages=find_packages(),
+                version='0.0.1',
+                description='Parsing MS data from Waters.',
+                long_description='Parsing MS data from Waters.',
+                author='Mateusz Krzysztof Łącki',
+                author_email='matteo.lacki@gmail.com',
+                url='https://github.com/MatteoLacki/waters/',
+                # download_url='https://github.com/MatteoLacki/MassTodonPy/tree/GutenTag',
+                keywords=['Mass Spectrometry',
+                          'Waters'],
+                classifiers=[
+                    'Development Status :: 1 - Planning',
+                    'License :: OSI Approved :: BSD License',
+                    'Intended Audience :: Science/Research',
+                    'Topic :: Scientific/Engineering :: Chemistry',
+                    'Programming Language :: Python :: 3.7'],
+                install_requires=['pandas'])
+
+if platform.system() == 'Windows':
+    settings['scripts'] = ["bin/iadbs2stats.py", "bin/iadbs2csv.py"]
+else:
+    settings['scripts'] = ["bin/iadbs2stats", "bin/iadbs2csv"]
+
+setup(**settings)

d323313b03edc2911debd691b6c3fda4f78b8ee1
matteo <matteo.lacki@gmail.com>
2020-06-05 09:39:41 +0200

    in search of a good data-frame diff...

--- waters/dev/ia_workflow.py
+++ waters/dev/ia_workflow.py
@@ -1,5 +1,6 @@
 %load_ext autoreload
 %autoreload 2
+from collections import Counter
 from pathlib import Path
 import pandas as pd
 pd.set_option('display.max_columns', 100)
@@ -15,12 +16,27 @@ files = list(data_f.glob('*IA*.xml'))
 I0 = iaDBsXMLparser(files[0])
 I1 = iaDBsXMLparser(files[1])
 
+diff = lambda d0, d1: (d1 != d0).any(1)
+
 I0.get_all_tag_counts()
 I0.parameters()
-I0.products()
+PR0 = I0.products()
+PR1 = I1.products()
+sum(diff(PR0, PR1))
+PR0[diff(PR0, PR1)]
+sum(diff(PR0.iloc[:,:14], PR1.iloc[:,:14]))
+Counter(PR0.LOSS_TYPE)
+Counter(PR1.LOSS_TYPE) # dass siehst gut aus.
+
+
+
 I0.proteins()
 I0.peptides()
-I0.query_masses()
+QM0 = I0.query_masses()
+QM1 = I1.query_masses()
+sum(diff(QM0, QM1))
+
+
 
 hit = I0.root.iter('HIT')
 a = next(hit)
@@ -56,3 +72,9 @@ D0 = pd.DataFrame(iterrator(I0))
 D1 = pd.DataFrame(iterrator(I1))
 # Need to compare those!
 # pd.concat([D0,D1]).drop_duplicates(keep=False)
+
+d0 = D0[(D0 != D1).any(1)]
+d1 = D1[(D0 != D1).any(1)]
+
+d0.iloc[:,1]
+d1.iloc[:,1]
